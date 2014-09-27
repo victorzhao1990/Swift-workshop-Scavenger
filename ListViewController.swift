019e334287ab2e2deab2161ffb82971240bd7967
Victor Zhao <xinyunzhao@msn.com>
2014-09-26 20:16:06 -0400

    Everything learnt from the workshop

---
+++ ListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ListViewController.swift
+//  Scavenger
+//
+//  Created by Xinyun (Victor) Zhao on 9/23/14.
+//  Copyright (c) 2014 Xinyun (Victor) Zhao. All rights reserved.
+//
+
+import Foundation
---
+++ Scavenger/ScavengerHuntItem.swift
@@ -0,0 +1,9 @@
+//
+//  ScavengerHuntItem.swift
+//  Scavenger
+//
+//  Created by Xinyun (Victor) Zhao on 9/23/14.
+//  Copyright (c) 2014 Xinyun (Victor) Zhao. All rights reserved.
+//
+
+import Foundation
